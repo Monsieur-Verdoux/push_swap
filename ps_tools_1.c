9306ac019fa27f7546c77565297a5a5dfb373230
Andrey Kovalevskiy <akovalev@student.hive.fi>
2024-01-26 14:36:17 +0200

    added bigger than int handling, empty sstring handling, fixed check for minus signs to catch ones not in the 0th position

--- ps_tools_1.c
+++ ps_tools_1.c
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:31:58 by akovalev          #+#    #+#             */
-/*   Updated: 2024/01/25 18:24:00 by akovalev         ###   ########.fr       */
+/*   Updated: 2024/01/26 14:31:58 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,23 @@ void	free_split(char **arr)
 	free(arr);
 }
 
+int	check_duplicates(t_vec *a, int num)
+{
+	size_t	j;
+
+	j = -1;
+	while (++j < a->len)
+		if (num == vec_int(a, j))
+			return (0);
+	vec_push(a, &num);
+	return (1);
+}
+
 int	build_vector(const char **arr, t_vec *a, bool check)
 {
 	size_t	i;
 	size_t	j;
+	long	temp_num;
 	int		num;
 
 	i = 1;
@@ -38,15 +51,16 @@ int	build_vector(const char **arr, t_vec *a, bool check)
 	{
 		j = -1;
 		while (++j < ft_strlen(arr[i]))
-			if (((arr[i][j] < 48 || arr[i][j] > 57) && arr[i][0] != 45) \
-			|| (arr[i][0] == 45 && ft_strlen(arr[i]) == 1))
+			if (((arr[i][j] < 48 || arr[i][j] > 57) && arr[i][0] != 45) || \
+			(arr[i][0] == 45 && ft_strlen(arr[i]) == 1) || \
+				(arr[i][j] == 45 && j != 0))
 				return (0);
-		num = ft_atoi(arr[i]);
-		j = -1;
-		while (++j < a->len)
-			if (num == vec_int(a, j))
-				return (0);
-		vec_push(a, &num);
+		temp_num = ft_atoi_long(arr[i]);
+		if (temp_num > 2147483647 || temp_num < -2147483648)
+			return (0);
+		num = (int)temp_num;
+		if (!check_duplicates(a, num))
+			return (0);
 		i++;
 	}
 	return (1);
@@ -56,6 +70,8 @@ int	split_arg_string(const char **argv, t_vec *a)
 {
 	char	**ptr;
 
+	if (!argv[1][0])
+		return (0);
 	ptr = ft_split(argv[1], ' ');
 	if (!ptr)
 	{
@@ -82,6 +98,8 @@ int	process_arguments(int argc, const char **argv, t_vec *a)
 	{
 		if (!split_arg_string(argv, a))
 			return (0);
+		else
+			return (1);
 	}
 	else if (!build_vector(argv, a, 1))
 		return (0);
